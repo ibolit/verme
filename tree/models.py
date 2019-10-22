1211301729591305f23a4c5fe5cfbcdf3576c315
Timofey Danshin <t_danshin@gcore.lu>
2019-10-22 10:33:28 +0300

    Add getting subtree by id

--- tree/models.py
+++ tree/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 
+class TreeManager(models.Manager):
+    def get_subtree_dict(self, root):
+        queryset = self.get_queryset().filter(pk=root)
+        queryset.prefetch_related('tree_set')
+        if not queryset:
+            raise Tree.DoesNotExist(root)
+        node = queryset[0]
+        subtree = dict(
+            id=root,
+            name=node.name,
+            children=[self.get_subtree_dict(child.pk) for child in node.tree_set.all()]
+        )
+        return subtree
+
+
 class Tree(models.Model):
     parent = models.ForeignKey(
         'self', models.CASCADE, null=True, verbose_name='Родитель')
     name = models.CharField(max_length=255, verbose_name='Название')
+
+    objects = TreeManager()
--- tree/tests.py
+++ tree/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from tree.models import Tree
+
+
+class TestTreeModel(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        t0 = Tree(name='Корень', parent=None)
+        t0.save()
+        t1 = Tree(name='Ветвь 1', parent=t0)
+        t1.save()
+        t2 = Tree(name='Ветвь 2', parent=t0)
+        t2.save()
+        t1_1 = Tree(name='Подветвь 1', parent=t1)
+        t1_1.save()
+
+    def test_get_subtree(self):
+        subtree = Tree.objects.get_subtree_dict(1)
+        self.assertDictEqual(
+            subtree,
+            {'id': 1, 'name': 'Корень', 'children':
+                [
+                    {'id': 2, 'name': 'Ветвь 1', 'children':
+                        [
+                            {'id': 4, 'name': 'Подветвь 1', 'children': []}
+                        ]
+                    },
+                    {'id': 3, 'name': 'Ветвь 2', 'children': []}
+                ]
+            }
+        )
+
+    def test_get_nonexisting_subtree(self):
+        self.assertRaises(Tree.DoesNotExist, Tree.objects.get_subtree_dict, 8)
